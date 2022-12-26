55a53ee8b8b77a1c930ea6139a83d13c45962fe1
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2022-12-26 14:31:30 +0200

    Initial

--- app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceTransitionsJobIntentService.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceTransitionsJobIntentService.kt
@@ -4,13 +4,9 @@ import android.content.Context
 import android.content.Intent
 import androidx.core.app.JobIntentService
 import com.google.android.gms.location.Geofence
-import com.udacity.project4.locationreminders.data.dto.ReminderDTO
-import com.udacity.project4.locationreminders.data.dto.Result
-import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
-import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
-import com.udacity.project4.utils.sendNotification
-import kotlinx.coroutines.*
-import org.koin.android.ext.android.inject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlin.coroutines.CoroutineContext
 
 class GeofenceTransitionsJobIntentService : JobIntentService(), CoroutineScope {
@@ -22,7 +18,7 @@ class GeofenceTransitionsJobIntentService : JobIntentService(), CoroutineScope {
     companion object {
         private const val JOB_ID = 573
 
-        //        TODO: call this to start the JobIntentService to handle the geofencing transition events
+        // TODO: call this to start the JobIntentService to handle the geofencing transition events
         fun enqueueWork(context: Context, intent: Intent) {
             enqueueWork(
                 context,
@@ -38,31 +34,30 @@ class GeofenceTransitionsJobIntentService : JobIntentService(), CoroutineScope {
         //TODO call @sendNotification
     }
 
-    //TODO: get the request id of the current geofence
+    // TODO: get the request id of the current geofence
     private fun sendNotification(triggeringGeofences: List<Geofence>) {
         val requestId = ""
 
-        //Get the local repository instance
-        val remindersLocalRepository: ReminderDataSource by inject()
-//        Interaction to the repository has to be through a coroutine scope
-        CoroutineScope(coroutineContext).launch(SupervisorJob()) {
-            //get the reminder with the request id
-            val result = remindersLocalRepository.getReminder(requestId)
-            if (result is Result.Success<ReminderDTO>) {
-                val reminderDTO = result.data
-                //send a notification to the user with the reminder details
-                sendNotification(
-                    this@GeofenceTransitionsJobIntentService, ReminderDataItem(
-                        reminderDTO.title,
-                        reminderDTO.description,
-                        reminderDTO.location,
-                        reminderDTO.latitude,
-                        reminderDTO.longitude,
-                        reminderDTO.id
-                    )
-                )
-            }
-        }
+        // Get the local repository instance
+//        val remindersLocalRepository: ReminderDataSource by inject()
+//        // Interaction to the repository has to be through a coroutine scope
+//        CoroutineScope(coroutineContext).launch(SupervisorJob()) {
+//            //get the reminder with the request id
+//            val result = remindersLocalRepository.getReminder(requestId)
+//            if (result is Result.Success<ReminderDTO>) {
+//                val reminderDTO = result.data
+//                //send a notification to the user with the reminder details
+//                sendNotification(
+//                    this@GeofenceTransitionsJobIntentService, ReminderDataItem(
+//                        reminderDTO.title,
+//                        reminderDTO.description,
+//                        reminderDTO.location,
+//                        reminderDTO.latitude,
+//                        reminderDTO.longitude,
+//                        reminderDTO.id
+//                    )
+//                )
+//            }
+//        }
     }
-
 }
